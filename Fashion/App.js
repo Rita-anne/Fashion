b22b83a2a80d5fd26bd74de33e98aa751f6db119
Rita-anne <annegakiza@gmail.com>
2024-02-20 15:36:17 +0200

    fff
    fff

---
+++ Fashion/App.js
@@ -0,0 +1,15 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { Cards } from './Cards';
+
+
+export default function App() {
+  return (
+    <Cards />
+  );
+}
+
+const styles = StyleSheet.create({
+ 
+  
+});
